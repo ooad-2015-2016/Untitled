a37a8bd7b174e8b269d087125ae887f2210b59e6
Dzana Hanic <dhanic1@etf.unsa.ba>
2016-04-24 16:07:09 +0200

    sobaListView cs i xaml

--- OOADProjekat/ProjekatOOAD/ProjekatOOAD/App.xaml.cs
+++ OOADProjekat/ProjekatOOAD/ProjekatOOAD/App.xaml.cs
@@ -16,6 +16,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using ProjekatOOAD.RRentingBaza.Views;
 
 namespace ProjekatOOAD
 {
@@ -83,7 +84,7 @@ namespace ProjekatOOAD
                 // When the navigation stack isn't restored navigate to the first page,
                 // configuring the new page by passing required information as a navigation
                 // parameter
-                rootFrame.Navigate(typeof(MainPage), e.Arguments);
+                rootFrame.Navigate(typeof(SobaListView), e.Arguments);
             }
             // Ensure the current window is active
             Window.Current.Activate();
