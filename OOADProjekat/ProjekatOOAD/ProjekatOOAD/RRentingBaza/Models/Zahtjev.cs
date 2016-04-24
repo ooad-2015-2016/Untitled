6025108b2ca687d3c1d6c89af27bda5bc4551455
lkesko1 <lkesko1@etf.unsa.ba>
2016-04-24 16:24:14 +0200

    Dodala klase Zahtjev.cs i ZahtjevDbContext.cs

---
+++ OOADProjekat/ProjekatOOAD/ProjekatOOAD/RRentingBaza/Models/Zahtjev.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RrentingProjekatBaza.ZahtjevBaza.Models
+{
+    class Zahtjev
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ZahtjevId { get; set; }//primary key u bazi
+        public string fourSqaureId { get; set; }//trebati ce za sihronizaciju kasnije
+        public string nazivZahtjeva { get; set; }//naziv zahtjeva
+        public int brojSobe { get; set; } //broj sobe koja ima neki zahtjev
+        public bool obavljenZahtjev { get; set; } //oznacava da li je zahtjev obavljen 
+    }
+}
