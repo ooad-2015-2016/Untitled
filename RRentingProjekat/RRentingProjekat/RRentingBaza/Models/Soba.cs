933f84d47c87f715edfb66dceefaf0a66ba14f3c
Dzana Hanic <dhanic1@etf.unsa.ba>
2016-04-26 14:31:33 +0200

    Novi projekat napravljen zbog greske pri migraciji

---
+++ RRentingProjekat/RRentingProjekat/RRentingBaza/Models/Soba.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace RRentingProjekat.RRentingBaza.Models
+{
+    public enum StatusSobe {Slobodna, Zauzeta, Rezervisana}
+
+    class Soba
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int SobaId { get; set; } //primary key
+        public string fourSquareId { get; set; } //za sinhronizaciju
+
+        public int BrojSobe { get; set; }
+        public StatusSobe Status { get; set; }
+        public float CijenaSobe { get; set; } //cijena sobe
+        public byte[] SlikaSobe { get; set; } //pocetna slika sobe
+        public int BrojKreveta { get; set; } //broj kreveta u sobi
+        public List<DateTime> RezervisaniDatumi { get; set; } //datumi kad je soba rezervisana
+    }
+}
