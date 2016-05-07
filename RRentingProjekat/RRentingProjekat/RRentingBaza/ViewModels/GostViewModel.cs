d6de6c0ade096ceb54f17418ec3678ad7153363b
lkesko1 <lkesko1@etf.unsa.ba>
2016-05-07 15:33:10 +0200

    dodala GostViewModel i ZahtjevViewModel

---
+++ RRentingProjekat/RRentingProjekat/RRentingBaza/ViewModels/GostViewModel.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RRentingProjekat.RRentingBaza.Models;
+using RRentingProjekat.RRentingBaza.Helper;
+using System.Windows.Input;
+using RRentingProjekat.RRentingBaza.Views;
+using RRentingProjekat.RRentingBaza.Forms;
+using Windows.UI.Xaml;
+
+namespace RRentingProjekat.RRentingBaza.ViewModels
+{
+    class GostViewModel 
+    {
+        public Gost gost { get; set;  }
+        public Soba soba { get; set;  }
+
+        public string Poruka { get; set; }
+        public INavigacija NavigationServis { get; set; }
+        public ICommand DodajZahtjev{ get; set; }
+        public ICommand OcijeniRRenting { get; set; }
+        public ICommand Izlaz { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged(String info)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(info));
+            }
+        }
+
+        public GostViewModel()
+        {
+            NavigationServis = new NavigationService();
+
+            DodajZahtjev = new RelayCommand<object>(dodajZahtjev);
+            OcijeniRRenting = new RelayCommand<object>(dodajStatistiku);
+            Izlaz = new RelayCommand<object>(izlaz);
+        }
+
+        private void dodajZahtjev(object parametar)
+        {
+            NavigationServis.Navigate(typeof(ZahtjeviView), new ZahtjevViewModel(this));
+        }
+
+        private void dodajStatistiku(object parametar)
+        {
+
+        }
+
+        private void izlaz(object parametar)
+        {
+            Application.Current.Exit();
+        }
+    }
+}
---
+++ RRentingProjekat/RRentingProjekat/RRentingBaza/ViewModels/ZahtjevViewModel.cs
@@ -0,0 +1,79 @@
+﻿using RRentingProjekat.RRentingBaza.Helper;
+using RRentingProjekat.RRentingBaza.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Windows.UI.Xaml.Controls;
+
+namespace RRentingProjekat.RRentingBaza.ViewModels
+{
+    class ZahtjevViewModel : UserControl
+    {
+        Zahtjev zahtjev { get; set; }
+        List<Zahtjev> listaZahtjeva;
+        Soba soba { get; set; }
+        Gost gost { get; set; }
+
+
+        public string Poruka { get; set; }
+        public INavigacija NavigationServis { get; set; }
+        public ICommand DodajZahtjev { get; set; }
+        public ICommand Nazad { get; set; }
+
+       
+
+        public ZahtjevViewModel(GostViewModel gostVM)
+        {
+            DataContext = new ZahtjeviView();
+            zahtjev = new Zahtjev();
+            soba = gostVM.soba;
+            gost = gostVM.gost;
+
+            DodajZahtjev = new RelayCommand<object>(dodajZahtjev);
+            Nazad = new RelayCommand<object>(nazad);
+
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged(String info)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(info));
+            }
+        }
+        private void dodajZahtjev(object parametar)
+        {
+       
+            //treba ispitati koji su checkboxovi chekirani i u skladu s tim te zahtjeve dodati u listView
+            using (var db = new RRentingDbContext())
+            {
+                soba = db.Sobe.Where(x => x.BrojSobe == gost.soba.BrojSobe).FirstOrDefault();
+
+                if (listaZahtjeva.Count == 0)
+                {
+                    Poruka = "Niste dodali ni jedan zahtjev.";
+                    NotifyPropertyChanged("Poruka");
+                }
+                else
+                {
+                    Poruka = "Zahtjevi su poslani";
+                    NotifyPropertyChanged("Poruka");
+                    // kako proslijediti ovu listu zahtjeva osoblju?
+                    NavigationServis.Navigate(typeof(GostView), new GostViewModel());
+                }
+            }
+        }
+
+        private void nazad(object parametar)
+        {
+     
+            NavigationServis.Navigate(typeof(GostView), new GostViewModel());
+        }
+    }
+}
