5c020a14a77b0982324499ac32e480d6dfe4a6ad
dljevo1 <dljevo1@etf.unsa.ba>
2016-05-08 10:46:49 +0200

    recepcionermodelview

---
+++ RRentingProjekat/RRentingProjekat/RRentingBaza/ViewModels/RecepcionerViewModel.cs
@@ -0,0 +1,39 @@
+﻿using RRentingProjekat.RRentingBaza.Helper;
+using RRentingProjekat.RRentingBaza.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace RRentingProjekat.RRentingBaza.ViewModels
+{
+    class RecepcionerViewModel
+    {
+        //public string Validacija { get; set; }
+
+
+        public INavigacija NavigationServis { get; set; }
+        public ICommand LoginKorisnika { get; set; }
+        //public RecepcionerViewModel(PrijavaViewModel pvm)
+        //{
+        //    Recepcioner = pvm.Recepcioner;
+        //}
+
+        public RecepcionerViewModel()
+        {
+            NavigationServis = new NavigationService();
+            //            logout = new RelayCommand<object>(logout);
+        }
+        private void logout(object parametar)
+        {
+            //using (var db = new RRentingDbContext())
+
+            //          NavigationServis.Navigate(typeof(Login), new PrijavaViewModel(this));
+
+
+
+        }
+    }
+}
---
+++ RRentingProjekat/RRentingProjekat/RRentingBaza/Views/RecepcionerListView.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace RRentingProjekat.RRentingBaza.Views
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class RecepcionerListView : Page
+    {
+        public RecepcionerListView()
+        {
+            this.InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            throw new NotImplementedException();
+        }
+        //private async void btn_Click(object sender, RoutedEventArgs e)
+        //{
+        //    var dialog = new MessageDialog("500KM");
+        //    await dialog.ShowAsync();
+        //}
+
+
+
+
+    }
+}
+
