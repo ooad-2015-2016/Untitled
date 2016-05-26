28b39431f7a467a3e81f3c567d05a04eb406a3ca
Dzana Hanic <dhanic1@etf.unsa.ba>
2016-05-26 14:26:10 +0200

    validirani neki dijelovi signup forme

--- RRentingProjekat/RRentingProjekat/RRentingBaza/ViewModels/RegistracijaViewModel.cs
+++ RRentingProjekat/RRentingProjekat/RRentingBaza/ViewModels/RegistracijaViewModel.cs
@@ -10,6 +10,7 @@ using System.Windows.Input;
 using RRentingProjekat.RRentingBaza.Views;
 using RRentingProjekat.RRentingBaza.DataSource;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Popups;
 
 namespace RRentingProjekat.RRentingBaza.ViewModels
 {
@@ -20,13 +21,14 @@ namespace RRentingProjekat.RRentingBaza.ViewModels
 
         public int Id { get; set; }
         public Gost RegistrovaniKorisnik { get; set; }
-        //public string Validacija { get; set; }
         public string UIme { get; set; }
         public string UPrezime { get; set; }
         public string UAdresa { get; set; }
         public string UTelefon { get; set; }
         public string UEmail { get; set; }
         public string UPassword { get; set; }
+
+        public string ValidacijaPoruka { get; set; }
 
         public INavigacija NavigationServis { get; set; }
         public ICommand SignupKorisnika { get; set; }
@@ -42,20 +44,67 @@ namespace RRentingProjekat.RRentingBaza.ViewModels
             UTelefon = "";
             SignupKorisnika = new RelayCommand<object>(signup, mozeLiSeRegistrovati);
             this.Id = System.Threading.Interlocked.Increment(ref m_Counter);
+
+            ValidacijaPoruka = "";
         }
         public bool mozeLiSeRegistrovati(object parametar)
         {
             return true;
         }
-        private void signup(object parametar)
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged(String info)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(info));
+            }
+        }
+        private async void signup(object parametar)
         {
             var UnosPassBox1 = parametar as PasswordBox;
             UPassword = UnosPassBox1.Password;
-            //using (var db = new RRentingDbContext())
-            RegistrovaniKorisnik = new Gost(Id, UIme, UPrezime, UTelefon, UAdresa, UPassword, UEmail, 0);
-            NavigationServis.Navigate(typeof(RezervacijaView), new RezervacijaViewModel(this));
+            //using (var db = new RRentingDbContext()) { --> validacija }
 
 
+            //validacija:
+            if (UIme.Length == 0 || UPrezime.Length == 0 || UAdresa.Length == 0 || UTelefon.Length == 0 || UPassword.Length == 0 || UEmail.Length == 0)
+            {
+                var dialog = new MessageDialog("Unesite sve tražene podatke", "Neuspješna prijava");
+                await dialog.ShowAsync();
+            }
+            else if (UIme.Length < 3 || UPrezime.Length < 3 || UAdresa.Length < 3)
+            {
+                var dialog = new MessageDialog("Prekratki su ime/prezime/adresa.", "Neuspješna prijava");
+                await dialog.ShowAsync();
+            }
+            else if (UTelefon.Length < 6)
+            {
+
+                var dialog = new MessageDialog("Neispravan format telefona", "Neuspješna prijava");
+                await dialog.ShowAsync();
+            }
+        
+            else if (UPassword.Length < 4 || !UEmail.Contains("@") || !UEmail.Contains("."))
+            {
+               
+                var dialog = new MessageDialog("Password je prekratak/Email nije ispravan.", "Neuspješna prijava");
+                await dialog.ShowAsync();
+            }
+
+            else
+            {
+                var dialog = new MessageDialog("Prijava uspješno završena.Dobrodošli!", "Uspješna prijava");
+                await dialog.ShowAsync();
+
+                //using (var db = new RRentingDbContext())
+                // db.Korisnici.Add(novi);
+                //db.SaveChanges();
+                RegistrovaniKorisnik = new Gost(Id, UIme, UPrezime, UTelefon, UAdresa, UPassword, UEmail, 0);
+
+                NavigationServis.Navigate(typeof(RezervacijaView), new RezervacijaViewModel(this));
+            }
+               
 
         }
     }
